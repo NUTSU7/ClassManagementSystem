f39f6c260ec6b7f6bdac23b900296a6069a7733e
NUTSU7 <bivolliviu62@gmail.com>
2021-04-28 21:06:52 +0300

    Introduced init options
    Signed-off-by: NUTSU7 <bivolliviu62@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,17 +3,44 @@
 
 using namespace std;
 
+void LogIn();
 void Register();
+void secret();
+
+class student
+{
+
+};
 
 int main()
 {
-    Register();
+    int init_choice;
+    cout << "\t\t--------------------------------------------" << endl;
+    cout << "\t\t\t\t1.Log In" << endl;
+    cout << "\t\t\t\t2.Register" << endl;
+    cout << "\t\t\t\t3.Exit" << endl;
+    cout << "\t\t--------------------------------------------" << endl;
+    cout << "\t\t\t\tYour choice- " << endl;
+    cin >> init_choice;
+    switch(init_choice)
+    {
+    case 1:
+        LogIn();
+    case 2:
+        Register();
+    case 3:
+        exit;
+    case 10:
+        secret();
+    }
     return 0;
 }
 
-void Register()
+void LogIn()
 {
     cout << "\t\t--------------------------------------------" << endl;
+    cout << "\t\t\tLog in with an authorised account" << endl;
+    cout << "\t\t--------------------------------------------" << endl;
     cout << "\t\t\t\tNAME- ";
-    cin >>
+    //cin >>
 }
