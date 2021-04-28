53fe14b9562cfa126014fffe9921daf59fcbe353
NUTSU7 <bivolliviu62@gmail.com>
2021-04-28 20:53:19 +0300

    Start
    Signed-off-by: NUTSU7 <bivolliviu62@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void Register();
+
+int main()
+{
+    Register();
+    return 0;
+}
+
+void Register()
+{
+    cout << "\t\t--------------------------------------------" << endl;
+    cout << "\t\t\t\tNAME- ";
+    cin >>
+}
