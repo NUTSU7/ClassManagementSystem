40f3cf77237a6af6316d74bf6d60f9fe41ae89bb
NUTSU7 <bivolliviu62@gmail.com>
2021-04-28 21:15:51 +0300

    Completed LogIn layout
    Signed-off-by: NUTSU7 <bivolliviu62@gmail.com>

--- main.cpp
+++ main.cpp
@@ -9,18 +9,20 @@ void secret();
 
 class student
 {
-
+public:
+    string name;
 };
 
 int main()
 {
+    system("clear");
     int init_choice;
     cout << "\t\t--------------------------------------------" << endl;
     cout << "\t\t\t\t1.Log In" << endl;
     cout << "\t\t\t\t2.Register" << endl;
     cout << "\t\t\t\t3.Exit" << endl;
     cout << "\t\t--------------------------------------------" << endl;
-    cout << "\t\t\t\tYour choice- " << endl;
+    cout << "\t\t\t\tYour choice- ";
     cin >> init_choice;
     switch(init_choice)
     {
@@ -38,9 +40,24 @@ int main()
 
 void LogIn()
 {
+    string temp_name, temp_pass;
+    system("clear");
     cout << "\t\t--------------------------------------------" << endl;
     cout << "\t\t\tLog in with an authorised account" << endl;
     cout << "\t\t--------------------------------------------" << endl;
-    cout << "\t\t\t\tNAME- ";
-    //cin >>
+    cout << "\t\t\t\tName- ";
+    cin >> temp_name;
+    cout << "\n\t\t\t\tPass- ";
+    cin >> temp_pass;
+    cout << "\t\t--------------------------------------------" << endl;
+}
+
+void Register()
+{
+    system("clear");
+}
+
+void secret()
+{
+    system("clear");
 }
